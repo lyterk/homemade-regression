f992d815f35c7f6acb54e6ace9d7c2c34308c0c2
Kevin Lyter <lyterk@gmail.com>
2016-02-18 20:58:41 -0800

    Notes and small changes to code

--- src/main/scala/regression.scala
+++ src/main/scala/regression.scala
@@ -2,7 +2,7 @@ package com.lyterk.LR
 
 import scala.math.pow
 
-object LRegression extends App {
+object LinearRegression extends App {
 
   val x: List[Double] = List(65.21, 64.75, 65.26, 65.76, 65.96)
   val y: List[Double] = List(67.25, 66.39, 66.12, 65.70, 66.64)
@@ -10,7 +10,7 @@ object LRegression extends App {
   /* This is a good direction to go for library writing, but for now I'm just going to assume that I'm taking a list of doubles.
   def types(l: List[T]): List[Double] = T match { // Also work on taking data types other than list.
     case i: List[Double] => i
-    case j: List[_] => i.map(_.toDouble) // Bit of a hammer. Will work on more diverse situations later.
+    case j: List[_] => i.map(_.toDouble) 
     }
    */
 
@@ -28,14 +28,14 @@ object LRegression extends App {
     z.map(i => (i._1 - mk) * (i._2 - ml)).sum / (x.length - 1)
   }
 
-  /** Takes two list variables of type Double, and returns values in tuples pertinent to a linear regression. The values returned are: (Intercept, Slope) (more to be added as determined necessary) */
+  /** Takes two list variables of type Double, and returns values in tuples pertinent to a linear regression. The values returned are: (Intercept, Slope) */
   def linearRegression(k: List[Double], l: List[Double]): (Double, Double) = {
     val m = k.zip(l)
     val mk = mean(k)
     val ml = mean(l)
     val slope = (m.map{case (a,b) => a * b}.sum - k.length * mk * ml) / (k.map(i => i * i).sum - k.length * mk * mk)
     val intercept = ml - slope * mk    
-    (slope, intercept)
+    (intercept, slope)
   }
 
   println(linearRegression(x,y))
